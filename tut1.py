a1fb07cd9fd9c357a596dde356cdd9c52dbeed0e
Arnab Mitra <arnabmitra471@gmail.com>
2024-01-24 22:42:35 +0530

    Having an intro to numpy

---
+++ tut1.py
@@ -0,0 +1,9 @@
+import  numpy as np
+
+l1 = [15,17,19,10,65,12]
+print(l1)
+print(type(l1))
+
+arr = np.array([34,19,54,76,78,76,54,76,87,76,123,87,190,123,76,12,76,190,200])
+print(arr)
+print(type(arr))
